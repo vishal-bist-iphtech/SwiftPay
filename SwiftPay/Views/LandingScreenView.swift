f573a7a9b82a005de5b64eeafc7f1cfc891d45a7
Vishal Bist <vishal.bist@iphtechnologies.com>
2026-09-18 18:30:01 +0530

    Added - custom theme colors

---
+++ SwiftPay/Views/LandingScreenView.swift
@@ -0,0 +1,23 @@
+//
+//  LandingScreenView.swift
+//  SwiftPay
+//
+//  Created by iPHTech 34 on 18/09/26.
+//
+
+import SwiftUI
+
+struct LandingScreenView: View {
+    
+    var body: some View {
+        
+        Text("Landing Screen")
+            .font(.largeTitle)
+        
+    }
+}
+
+
+#Preview {
+    LandingScreenView()
+}
